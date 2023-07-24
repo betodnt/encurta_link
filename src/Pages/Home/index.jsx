05e56fc7b1cee24ab8a55db6118ffb8e06d4629d
betodnt <dentgm@gmail.com>
2023-07-24 17:20:26 -0300

    Update

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -47,7 +47,7 @@ export default function Home() {
   return (
     <div className="container-home">
       <div className="logo">
-        <img src="/encurta_link/public/Image/logo.svg" alt="Encurta Link Logo" />
+        <img src="./encurta_link/public/Image/logo.svg" alt="Encurta Link Logo" />
         <h1>Encurta Link</h1>
         <span>Cole seu link para encurtar 👇</span>
       </div>
